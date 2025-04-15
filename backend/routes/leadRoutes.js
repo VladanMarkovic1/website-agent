d9d0b7e2dbdbbffbf526a7e125109f9ce6a1994d
VladanMarkovic1 <vmarkovic984@gmail.com>
2025-04-15 14:14:57 +0200

    update imports for lead routes based on code refactoring for lead controller

--- backend/routes/leadRoutes.js
+++ backend/routes/leadRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { getLeads, createLeadHandler, updateLeadStatusHandler } from "../controllers/leadControllers/leadController.js";
+import { getLeads } from '../controllers/leadControllers/getLeadsController.js';
+import { createLeadHandler } from '../controllers/leadControllers/createLeadController.js';
+import { updateLeadStatus } from '../controllers/leadControllers/updateLeadStatusController.js';
 import { authenticateToken } from "../middleware/auth.js";
 import { checkBusinessOwner } from "../middleware/checkBusinessOwner.js";
 import { addNoteForLeadController } from "../controllers/leadControllers/addNoteForLeadController.js";
@@ -11,13 +13,10 @@ const router = express.Router();
 router.get("/:businessId", authenticateToken, checkBusinessOwner, getLeads);
 
 // POST /leads - Create a new lead
-// If lead creation should be public (e.g., from a chatbot), you might leave this unprotected.
-// But if you want only authenticated users to create leads, secure it too:
 router.post("/", authenticateToken, createLeadHandler);
 
 // PUT /leads/:businessId/:leadId - Update the status of a lead
-// This one should be secured so that only authorized users can update lead status.
-router.put("/:businessId/:leadId", authenticateToken, checkBusinessOwner, updateLeadStatusHandler);
+router.put("/:businessId/:leadId", authenticateToken, checkBusinessOwner, updateLeadStatus);
 
 // POST /leads/:businessId/notes/:leadId - Add a note to a lead
 router.post("/:businessId/notes/:leadId", authenticateToken, checkBusinessOwner, addNoteForLeadController);
