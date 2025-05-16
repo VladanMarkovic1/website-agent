32d33b55c7fa7c0c95e45e32b861a518568afa6f
VladanMarkovic1 <vmarkovic984@gmail.com>
2025-05-16 16:31:16 +0200

    changes for scraping

--- backend/scraper/scraper.js
+++ backend/scraper/scraper.js
@@ -45,6 +45,7 @@ const scrapeBusinessData = async (business) => {
     
     const browser = await puppeteer.launch({ 
         headless: true,
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
