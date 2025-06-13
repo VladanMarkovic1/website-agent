d8cf771d291f0e7fc8fe0b58dffba53a52fa360f
VladanMarkovic1 <vmarkovic984@gmail.com>
2025-06-13 13:23:08 +0200

    Enhance Selector schema to include detailed contact information
    - Introduce ContactSelectorSchema to store phone, email, and address fields.
    - Update contactSelector field in SelectorsSchema to utilize the new ContactSelectorSchema for improved data structure.

--- backend/models/Selector.js
+++ backend/models/Selector.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
+const ContactSelectorSchema = new mongoose.Schema({
+    phone: { type: String },
+    email: { type: String },
+    address: { type: String }
+}, { _id: false });
+
 const SelectorsSchema = new mongoose.Schema({
     businessId: { type: String, required: true, unique: true },
     serviceSelector: { type: String, required: true },
     aboutSelector: { type: String },
-    contactSelector: { type: String },
+    contactSelector: { type: ContactSelectorSchema },
     faqsSelector: { 
         question: { type: String },
         answer: { type: String }
